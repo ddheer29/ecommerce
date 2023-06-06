1a340333736e89e3b27953639877b2b82c7e5495
Divyang Dheer <88249922+ddheer29@users.noreply.github.com>
2023-06-06 18:56:13 +0530

    user profile update functionality added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { registerController, loginController, testController, forgotPasswordController } from '../controller/authController.js'
+import {
+    registerController,
+    loginController,
+    testController,
+    forgotPasswordController,
+    updateProfileController
+} from '../controller/authController.js'
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 
 // Router Object
@@ -27,5 +33,9 @@ router.get('/user-auth', requireSignIn, (req, res) => {
 router.get('/admin-auth', requireSignIn, isAdmin, (req, res) => {
     res.status(200).send({ ok: true });
 })
+
+// update profile
+router.put('/profile', requireSignIn, updateProfileController);
+
 
 export default router;
